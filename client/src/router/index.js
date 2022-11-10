7e258c973240528eefe4720c0f651c2887e0429d
rahfathoni <rahfathoni@gmail.com>
2022-11-10 20:43:38 +0700

    base app

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from '../views/dashboard/index.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Dashboard
   }
   // {
   //   path: '/about',
